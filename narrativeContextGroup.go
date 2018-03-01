9e61d354032b0c5b77bd23c00c5a4b12e4e8d758
Ingmar Drewing <ingmar@drewing.de>
2018-03-01 23:12:21 +0100

    Add hacky index page

--- narrativeContextGroup.go
+++ narrativeContextGroup.go
@@ -1,6 +1,9 @@
 package staticPresentation
 
-import "github.com/ingmardrewing/staticIntf"
+import (
+	"github.com/ingmardrewing/fs"
+	"github.com/ingmardrewing/staticIntf"
+)
 
 func NewNarrativeContextGroup(
 	pages []staticIntf.Page,
@@ -17,9 +20,24 @@ func NewNarrativeContextGroup(
 
 	cg := new(narrativeContextGroup)
 	cg.pagesContext = narrativeCtx
+	cg.dto = dto
 	return cg
 }
 
 type narrativeContextGroup struct {
 	abstractContextGroup
+	dto staticIntf.ContextDto
+}
+
+func (a *narrativeContextGroup) RenderPages(dir string) []fs.FileContainer {
+	fcs := a.pagesContext.RenderPages(dir)
+	if len(fcs) > 1 {
+		lastPageFc := fcs[len(fcs)-2]
+		index := fs.NewFileContainer()
+		index.SetData(lastPageFc.GetData())
+		index.SetPath(a.dto.TargetDir())
+		index.SetFilename("index.html")
+		return append(fcs, index)
+	}
+	return fcs
 }
