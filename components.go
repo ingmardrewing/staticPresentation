6098959df512e4705b849da7acae933aaefcdbd2
Ingmar Drewing <ingmar@drewing.de>
2018-03-12 20:46:57 +0100

    Fix url within disqus component

--- components.go
+++ components.go
@@ -529,7 +529,7 @@ func (dc *DisqusComponent) GetJs() string {
 
 func (dc *DisqusComponent) VisitPage(p staticIntf.Page) {
 	dc.configuredJs = fmt.Sprintf(`var disqus_config = function () { this.page.title= "%s"; this.page.url = '%s'; this.page.identifier =  '%s'; }; (function() { var d = document, s = d.createElement('script'); s.src = 'https://%s.disqus.com/embed.js'; s.setAttribute('data-timestamp', +new Date()); (d.head || d.body).appendChild(s); })();`, p.Title(),
-		dc.abstractComponent.context.GetSiteName()+p.PathFromDocRoot()+p.HtmlFilename(), p.DisqusId(), dc.abstractComponent.context.GetDisqusShortname())
+		p.Url(), p.DisqusId(), dc.abstractComponent.context.GetDisqusShortname())
 	n := htmlDoc.NewNode("div", " ", "id", "disqus_thread", "class", "disqus")
 	js := htmlDoc.NewNode("script", dc.configuredJs)
 	wn := dc.wrap(n)
