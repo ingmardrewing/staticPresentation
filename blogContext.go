d7c8a764dcc9de8d604a10a0bddcbba9b2fe7359
Ingmar Drewing <ingmar@drewing.de>
2019-04-06 14:17:02 +0200

    Refactor

--- blogContext.go
+++ blogContext.go
@@ -1,30 +1,38 @@
 package staticPresentation
 
 import (
-	"fmt"
 	"path"
 
 	"github.com/ingmardrewing/fs"
 	"github.com/ingmardrewing/staticIntf"
 	"github.com/ingmardrewing/staticModel"
-	"github.com/ingmardrewing/staticPersistence"
 	"github.com/ingmardrewing/staticUtil"
 )
 
-func NewBlogContext(s staticIntf.Site) staticIntf.Context {
-	tool := staticUtil.NewPagesContainerCollectionTool(s)
+func NewBlogContext(site staticIntf.Site) staticIntf.Context {
+	tool := staticUtil.NewPagesContainerCollectionTool(site)
 
 	cg := new(blogContext)
-	cg.site = s
+	cg.site = site
 
-	cg.renderer = NewBlogRenderer(s)
+	archivePage := cg.makeArchive()
+	site.AddMain(archivePage)
+
+	cg.archiveRenderer = NewBlogArchiveRenderer(site)
+	cg.archiveRenderer.Pages(archivePage)
+
+	cg.renderer = NewBlogRenderer(site)
 	cg.renderer.Pages(tool.GetPagesByVariant(staticIntf.BLOG)...)
 
-	cg.naviRenderer = NewBlogNaviRenderer(s)
+	cg.naviRenderer = NewBlogNaviRenderer(site)
 	cg.naviRenderer.Pages(tool.GetNaviPagesByVariant(staticIntf.BLOG)...)
 
-	cg.archiveRenderer = NewBlogArchiveRenderer(s)
-	cg.addArchiveLocation()
+	cg.rssRenderer = NewRssRenderer(
+		cg.getLastTenReversedPages(),
+		path.Join(site.TargetDir(), "/blog/"),
+		"/blog/",
+		site.RssFilename())
+
 	return cg
 }
 
@@ -32,23 +40,17 @@ type blogContext struct {
 	abstractContext
 	naviRenderer    staticIntf.Renderer
 	archiveRenderer staticIntf.Renderer
+	rssRenderer     staticIntf.Renderer
 }
 
-func (b *blogContext) addArchiveLocation() {
-	file := "archive.html"
-	path := "/blog/"
-	fullPath := path + file
-	archiveLoc := staticModel.NewLocation(
-		"",
-		b.site.Domain(),
-		"Archive",
-		"",
-		path,
-		file,
-		"",
-		fullPath,
-		fmt.Sprintf("https://%s%s", b.site.Domain(), fullPath))
-	b.site.AddMain(archiveLoc)
+func (b *blogContext) makeArchive() staticIntf.Page {
+	pm := staticModel.NewPageMaker()
+	pm.Title("Archive")
+	pm.Category("blog archive")
+	pm.PathFromDocRoot("/blog/")
+	pm.FileName("archive.html")
+	pm.Site(b.site)
+	return pm.Make()
 }
 
 func (b *blogContext) GetComponents() []staticIntf.Component {
@@ -60,37 +62,7 @@ func (b *blogContext) GetComponents() []staticIntf.Component {
 func (b *blogContext) RenderPages() []fs.FileContainer {
 	fcs := b.renderer.Render()
 	fcs = append(fcs, b.naviRenderer.Render()...)
-
-	rssFc := b.RenderRssPages()
-	if rssFc != nil {
-		fcs = append(fcs, rssFc)
-	}
-	fcs = append(fcs, b.RenderBlogArchive()...)
-
+	fcs = append(fcs, b.archiveRenderer.Render()...)
+	fcs = append(fcs, b.rssRenderer.Render()...)
 	return fcs
-}
-
-func (b *blogContext) RenderRssPages() fs.FileContainer {
-	rr := NewRssRenderer(
-		b.getLastTenReversedPages(),
-		path.Join(b.site.TargetDir(), "/blog/"),
-		"/blog/",
-		b.site.RssFilename())
-	return rr.Render()
-}
-
-func (b *blogContext) RenderBlogArchive() []fs.FileContainer {
-	dto := staticPersistence.NewFilledDto(
-		b.site.Domain()+" Archive",
-		"",
-		"",
-		"blog archive",
-		"",
-		"/blog/",
-		"archive.html",
-		[]string{},
-		[]staticIntf.Image{})
-	p := staticModel.NewPage(dto, b.site.Domain(), b.site)
-	b.archiveRenderer.Pages(p)
-	return b.archiveRenderer.Render()
 }
